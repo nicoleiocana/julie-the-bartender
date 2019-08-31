583ea6aaf34970f2f469eed67f87b60b03e7fbf4
Nicolei Ocana <nicoleiocana@gmail.com>
2019-08-31 00:57:09 +0000

    create featured menu page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   def home
   end
   
+  def featured_menu
+  end 
+  
   def accolades
   end 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get '/featured-menu', to: 'pages#featured_menu'
   get '/accolades', to: 'pages#accolades'
   resources :messages, only: [:new, :create]
   get '/tip_me', to: 'pages#tip_me'
